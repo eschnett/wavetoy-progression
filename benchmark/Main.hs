baf17d46cea87b01b24a95af8e9e9a8a297d5730
Erik Schnetter <schnetter@gmail.com>
2017-04-16 16:09:50 -0400

    Add benchmark

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,6 +1,15 @@
--- You can benchmark your code quickly and effectively with Criterion. See its
--- website for help: <http://www.serpentine.com/criterion/>.
+-- You can benchmark your code quickly and effectively with Criterion.
+-- See its website for help: <http://www.serpentine.com/criterion/>.
 import Criterion.Main
 
+import WaveToy1
+
+default (Int)
+
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = defaultMain [
+  -- bgroup "Grid" [bench "init" $ nf (initGrid (0.0::Double) (0.0, 1.0)) 1001]
+  bgroup "Grid" [bench "init" $
+                 whnf (\np -> let g = initGrid (0.0::Double) (0.0, 1.0) np
+                              in energyGrid g) 1001]
+  ]
